67ba315ac507eb99aa3a0a93417d72d0ad8d357e
Joana Favaretto <joana.favarettoo@edu.sc.senai.br>
2026-02-04 10:46:37 -0300

    fix: test render no Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <div style={{ padding: 24, fontSize: 24, color: "white" }}>
+      Oi! React renderizou ✅
+    </div>
   </React.StrictMode>
 );
