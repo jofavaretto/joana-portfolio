fb4321df3920886c84967375802e0d5a6bfb8cee
Joana Favaretto <joana.favarettoo@edu.sc.senai.br>
2026-02-04 11:24:12 -0300

    main ok

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div style={{
-      padding: 24,
-      fontSize: 28,
-      fontWeight: 700,
-      color: "#ffffff",
-      background: "#ff006e"
-    }}>
-      Oi! React renderizou ✅
-    </div>
+    <App />
   </React.StrictMode>
 );
